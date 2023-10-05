3541c2d09ef9fbc1fdee1903c3e797f12ce3254d
garm2005 <matgarlab@yahoo.com>
2023-10-05 13:23:48 -0600

    modifique los operadores de potencia

--- python/vectorfield.py
+++ python/vectorfield.py
@@ -1,4 +1,3 @@
- -*- coding: utf-8 -*-
 """
 Script que grafica un campo vectorial
 
@@ -8,6 +7,7 @@ Created on Thu Oct  5 2023
 """
 
 # Importando los modulos
+from mpl_toolkits.mplot3d import axes3d
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -28,3 +28,23 @@ plt.quiver(XX, YY, u, v)
 plt.xlabel('u')
 plt.ylabel('v')
 plt.show()
+
+# Caso 3D
+x = np.arange(-0.8, 1, 0.2)
+y = np.arange(-0.8, 1, 0.2)
+z = np.arange(-0.8, 1, 0.8)
+
+# Creando el dominio espacial
+[XX,YY,ZZ] = np.meshgrid(x,y,z)
+
+# Calculando las componentes del campo vectorial
+u = ZZ/(XX**2 + YY**2 + ZZ**2)
+v = XX/(XX**2 + YY**2 + ZZ**2)
+w = YY/(XX**2 + YY**2 + ZZ**2)
+
+# Visualizando el campo 
+fig = plt.figure()
+ax = fig.gca(projection='3d') 
+ax.quiver(XX, YY, ZZ, u, v, w, length=0.1, color='blue')
+plt.xlabel('u')
+plt.ylabel('v')
